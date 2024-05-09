34b4d3f56f24a9269abda55b98b898cae0784355
NtombiHontyo <u252121986@byui.edu>
2024-05-10 01:51:40 +0200

    error fixed

--- contollers/baseController.js
+++ contollers/baseController.js
@@ -2,7 +2,7 @@ const utilities = require("../utilities/")
 const baseController = {}
 
 baseController.buildHome = async function(req, res){
-   // const nav = await utilities.getNav()
+    const nav = await utilities.getNav()
     res.render("index", {title: "Home", nav})
 }
 
