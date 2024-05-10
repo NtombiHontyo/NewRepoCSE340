cdaf7cddbd86f1b20460743ea6931c40ae85e84f
NtombiHontyo <u252121986@byui.edu>
2024-05-10 07:26:48 +0200

    debug act

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  //if(err.status == 404) { message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  if(err.status == 404) { message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message: err.message,
