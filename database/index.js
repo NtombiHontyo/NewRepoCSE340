a23db16e26e35a9256846387bbf5b59ad7518c75
NtombiHontyo <u252121986@byui.edu>
2024-05-13 08:09:27 +0200

    minor change in db envs

--- database/index.js
+++ database/index.js
@@ -10,7 +10,7 @@ require("dotenv").config()
 let pool 
 if (process.env.NODE_ENV == "development") {
     pool = new Pool({
-        connectionString: process.env.DATABASE_URL_INTERNAL,
+        connectionString: process.env.DATABASE_URL,
         ssl: {
             rejectUnauthorized: false,
         },
